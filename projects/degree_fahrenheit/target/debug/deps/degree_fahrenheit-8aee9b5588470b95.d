dd3329c63d82aae8179b9e1bd32daaf15e4fafa4
Sachin@Linux <sac@Latitude-5420>
2025-04-21 01:20:26 +0200

    Added projects based on chapter 3

---
+++ projects/degree_fahrenheit/target/debug/deps/degree_fahrenheit-8aee9b5588470b95.d
@@ -0,0 +1,5 @@
+/home/sac/Desktop/Rustacean/projects/degree_fahrenheit/target/debug/deps/libdegree_fahrenheit-8aee9b5588470b95.rmeta: src/main.rs
+
+/home/sac/Desktop/Rustacean/projects/degree_fahrenheit/target/debug/deps/degree_fahrenheit-8aee9b5588470b95.d: src/main.rs
+
+src/main.rs:
---
+++ projects/degree_fahrenheit/target/debug/deps/degree_fahrenheit-c1855c8f56878992.d
@@ -0,0 +1,5 @@
+/home/sac/Desktop/Rustacean/projects/degree_fahrenheit/target/debug/deps/libdegree_fahrenheit-c1855c8f56878992.rmeta: src/main.rs
+
+/home/sac/Desktop/Rustacean/projects/degree_fahrenheit/target/debug/deps/degree_fahrenheit-c1855c8f56878992.d: src/main.rs
+
+src/main.rs:
